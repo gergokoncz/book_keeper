89f03144bac736dd0a4e2f216184a6dcdb15ca64
gergokoncz <gergokoncz06@gmail.com>
2023-11-06 00:04:02 +0100

    testing happy path properly for for authio

--- src/tests/test_authio.py
+++ src/tests/test_authio.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """Test module AuthIO."""
+import boto3
 import pytest
 import yaml
 from pytest import fixture
@@ -9,6 +10,17 @@ from yaml import SafeLoader
 from src.utils import AuthIO
 
 from .conftest import TEST_BUCKET_NAME, TEST_REGION, TEST_USERNAME
+
+
+@pytest.fixture(autouse=True)
+def before_and_after_test():
+    """Fixture to run before and after each test."""
+    # Run before test
+    yield
+    # Run after test
+    s3 = boto3.resource("s3", region_name=TEST_REGION)
+    bucket = s3.Bucket(TEST_BUCKET_NAME)
+    bucket.objects.all().delete()
 
 
 @fixture
@@ -55,6 +67,10 @@ def test_get_auth_config(test_auth_config):
     """
     authio = AuthIO(bucket=TEST_BUCKET_NAME)
 
-    result = authio.get_auth_config()
+    result = authio.update_auth_config(config=test_auth_config)
 
-    assert result == test_auth_config
+    assert result is True
+
+    authconfig = authio.get_auth_config()
+
+    assert authconfig == test_auth_config
